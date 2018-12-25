49f1c16375c47ca3a317ad8bcb82e777391e2bec
Shiv Shankar <shankar7shiv@gmail.com>
2018-12-25 07:25:21 +0000

    Add css styling to the index.html

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express"),
     todos = require("./routes/todos"),
     bodyParser = require("body-parser");
 app.use(express.static(__dirname+'/views'));
+app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req,res){
